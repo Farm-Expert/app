315d567d716dfd41894a4d0ae584563e0e77c609
Arjun Soni <94898141+ArjunKSoni@users.noreply.github.com>
2024-01-09 16:40:37 +0530

    api

---
+++ src/auth/auth.jsx
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+const API="farm-expert-app-backend-beige.vercel.app/"
+
+
+export const signup=async ({name,email,mobile,password,address,kisanid})=>{
+    const data=await axios.post(API+'auth/signup',{name,email,mobile,password,address,kisanid});
+    console.log(data.data);
+}
+
+export const login=async ({email,password})=>{
+    const data=await axios.post(API+'auth/signup',{email,password});
+    console.log(data.data);
+}
