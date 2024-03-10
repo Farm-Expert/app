16b3ae7cd3c0f56a872a8f3c82456ebafc68980a
ArjunKSoni <aksoni0520@gmail.com>
2024-03-11 01:23:04 +0530

    api integration

--- src/auth/recent.jsx
+++ src/auth/recent.jsx
@@ -1,11 +1,40 @@
 import axios from "axios";
 
-const API="farm-expert-app-backend-beige.vercel.app/"
+const API="http://myfarmexpert.tech:5050/"
 
 
-export const recentCrop=async ({cropname})=>{
-    const data=await axios.post(API+'recent/recent_crop');
-    console.log(data.data);
+export const recentCrop=async (token)=>{
+    try {
+        const headers = {
+            "authorization":`Bearer ${token}`
+        };
+        const data = await axios.get(API + 'recent/recent_crop', { headers });
+        if (data.data){
+            return data.data;
+        }
+        else{
+            return null
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export const add_recentCrop=async (token,cropname)=>{
+    try {
+        const headers = {
+            "authorization":`Bearer ${token}`
+        };
+        const data = await axios.post(API + 'recent/store_crop_name', {cropname}, { headers });
+        if (data.data){
+            return data.data.data;
+        }
+        else{
+            return null
+        }
+    } catch (error) {
+        console.log(error);
+    }
 }
 
 
