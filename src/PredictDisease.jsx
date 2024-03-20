32cfbcefca535c747a06d8f025941612d9869f70
shivangi-gupta-01 <110471751+shivangi-gupta-01@users.noreply.github.com>
2024-03-21 02:19:58 +0530

    Predict Disease Completed

--- src/PredictDisease.jsx
+++ src/PredictDisease.jsx
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, ImageBackground, StyleSheet, Image, ScrollView, TouchableOpacity, StatusBar } from 'react-native';
 import bgimage from '../assets/bg.webp';
-import CropScroll from './comp/CropScroll';
+import DiseaseScroll from './comp/DiseaseScroll';
 import back from "../assets/back.png"
+import { diseasePredict } from './auth/ml_api';
 
 
-export default function PredictDisease({navigation, route}) {
-    console.log(route.params);
+export default function PredictDisease({ navigation, route }) {
+  console.log(route.params);
   const diseaseImg = route.params.img;
-  console.log(diseaseImg,"Predict Disease");
+  const [name, setName]=useState("");
+  const [plantName, setPlantName]=useState("");
+  const [precautions,setPrecautions]= useState([]);
+  const [symptoms,setSymptoms]= useState([]);
+  const [treatment,setTreatment]= useState([]);
+  // console.log(diseaseImg, "Predict Disease");
+  useEffect(() => {
+    handleDisease();
+  }, [])
+  const handleDisease = async () => {
+    const data = await diseasePredict("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSnuwXIC8XWUG4JByPU7FteFFro1TWkw4Nuig&s");
+    console.log(data, "predict data")
+    if(data){
+      setName(data.disease_name);
+      setPlantName(data.plant_name);
+      setPrecautions(data.precautions);
+      setSymptoms(data.symptoms);
+      setTreatment(data.treatment);
+    }
+  }
+
   return (
     <ImageBackground
       source={bgimage}
@@ -17,17 +38,28 @@ export default function PredictDisease({navigation, route}) {
       <StatusBar barStyle="light-content" backgroundColor="transparent" />
       <View className="flex w-screen items-center justify-center flex-row top-0 m-0 p-0">
         <View className="w-screen items-center top-0 justify-center rounded-lg overflow-hidden">
-          <Image source={{uri:diseaseImg}} className="rounded-full h-full w-full m-0 p-0" style={styles.diseaseimg} />
+          <Image source={{ uri: diseaseImg }} className="rounded-full h-full w-full m-0 p-0" style={styles.diseaseimg} />
         </View>
       </View>
-      <Text className="text-white w-full -mt-5 font-bold text-center text-3xl">Disease Name</Text>
+      <Text className="text-white w-full -mt-5 font-bold text-center text-3xl">{name}</Text>
       <TouchableOpacity onPress={() => navigation.navigate("disease")} className="absolute w-10 left-5 flex items-center justify-center h-10 bg-white rounded-full top-12">
-                <Image source={back} style={{ width: 20, height: 20 }} />
+        <Image source={back} style={{ width: 20, height: 20 }} />
       </TouchableOpacity>
-      <View className="p-5 w-screen m-3 justify-center" style={{ height: 150 ,backgroundColor:'#ffffff60',borderTopLeftRadius: 50, borderTopRightRadius: 50}}>
-        <ScrollView bouncesZoom={true} showsHorizontalScrollIndicator={false} bounces={true}>
-            <Text>Lorem ipsum dolor sit, amet consectetur adipisicing elit. In ex provident et molestiae eius, ratione rem odio laborum consectetur ipsum dolore voluptatem tempora, ducimus dignissimos ut reiciendis eos quibusdam obcaecati?</Text>
+      <View className="p-3 w-screen m-3 items-center justify-center h-full" style={{ backgroundColor: '#ffffff60', borderTopLeftRadius: 50, borderTopRightRadius: 50 }}>
+        <ScrollView   bouncesZoom={true} showsVerticalScrollIndicator={false} bounces={true}>
+          <Text className="text-white w-full font-bold text-center text-xl">{plantName}</Text>
+          <View style={{ height: 2 }} className=" items-center w-1.3 bg-white m-2"></View>
           {/* Here Precuations and cure display */}
+          {/* {precautions.map((item,i)=>{
+            return (
+              <DiseaseScroll key={i} value={item}></DiseaseScroll>
+            )
+          })} */}
+          <View className=" flex flex-col justify-center items-center m-2 ">
+          <DiseaseScroll value={precautions} title="Precautions" />
+          <DiseaseScroll value={symptoms} title="Symptoms" />
+          <DiseaseScroll value={treatment} title="Treatment" /> 
+          </View>
         </ScrollView>
       </View>
 
@@ -38,25 +70,25 @@ export default function PredictDisease({navigation, route}) {
 }
 
 const styles = StyleSheet.create({
-    backgroundImage: {
-        flex: 1,
-        top: 0,
-        paddingTop: 50,
-        resizeMode: 'cover',
-        paddingHorizontal: 30,
-        alignItems: "center"
-    },
+  backgroundImage: {
+    flex: 1,
+    top: 0,
+    paddingTop: 50,
+    resizeMode: 'cover',
+    paddingHorizontal: 30,
+    alignItems: "center"
+  },
   diseaseimg: {
     justifyContent: 'center',
     height: 200,
     width: "50%",
     marginBottom: 40,
     // borderRadius: 50,
-    
+
     // resizeMode: "contain"
   },
   rec: {
-    bottom:50,
+    bottom: 50,
     height: 80,
     backgroundColor: "#F3EEEAA0",
     position: "absolute",
